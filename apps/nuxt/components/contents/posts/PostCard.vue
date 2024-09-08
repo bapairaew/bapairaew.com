eebbb3f9251d6c7e02ddd69f3f11b25a6b00667b
dom <iam@bapairaew.com>
2024-09-08 10:02:39 +0900

    fix type

--- apps/nuxt/components/contents/posts/PostCard.vue
+++ apps/nuxt/components/contents/posts/PostCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { Post } from "~/utils/data/posts";
+import type { Post } from "~/utils/data/data";
 import dateformat from "dateformat";
 
 const props = defineProps<{
